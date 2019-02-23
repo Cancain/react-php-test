832312cc1cb81403760911ba392123251cb7c2a9
Cancain <eriksson.tomas@gmail.com>
2019-02-23 21:37:41 +0100

    started work on the Modal

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,11 +2,13 @@ import React, { Fragment } from 'react';
 
 import News from '../../Components/News/News';
 import Header from '../../Containters/Header/Header';
+import Modal from '../../Containters/Modal/Modal';
 
 const layout = (props) => {
     return (
         <Fragment>
             <Header />
+            <Modal />
             <News />
         </Fragment>
     )
